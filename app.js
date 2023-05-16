ed01823afbb01a1e4d0521c5e015e1294478edc1
sandeep <sandeepvemula408@gmail.com>
2023-05-16 13:00:40 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,60 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname+"/date.js");
+
+const app = express();
+
+const items = ["buy food"];
+constworkItems = [];
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+
+const day = date.getDate();
+
+  res.render("list", {
+    listTitle: day,
+    newlistItems: items
+  });
+});
+
+app.post("/", function(req, res) {
+  const item = req.body.newItem;
+  if (req.body.list === "work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+     res.redirect("/");
+  }
+
+
+});
+
+app.get("/work", function(req, res) {
+  res.render("list", {
+    listTitle: "work List",
+    newlistItems: workItems
+  });
+})
+
+app.post("/work", function(req, res) {
+  const item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+})
+
+app.get("/about", function(req, res) {
+  res.render("about") ;
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000.");
+});
